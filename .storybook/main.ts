21c5ce2f255f3dc38b00a0171ad9b7233d0e8ebd
minyoung1101 <my11612@naver.com>
2025-11-05 15:50:27 +0900

    config svgr 기본 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import type { RuleSetRule } from "webpack";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -18,10 +19,33 @@ const config: StorybookConfig = {
   },
   staticDirs: ["../public"],
   webpackFinal: async (config) => {
+    // SVGR
+    if (config.module?.rules) {
+      const imageRule = config.module.rules.find(
+        (rule): rule is RuleSetRule =>
+          typeof rule === "object" &&
+          rule?.test instanceof RegExp &&
+          rule.test.test(".svg")
+      );
+      if (imageRule) {
+        if (!imageRule.exclude) {
+          imageRule.exclude = /\.svg$/i;
+        } else if (Array.isArray(imageRule.exclude)) {
+          imageRule.exclude.push(/\.svg$/i);
+        } else {
+          imageRule.exclude = [imageRule.exclude, /\.svg$/i];
+        }
+      }
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      });
+    }
+
     if (config.resolve) {
       config.resolve.alias = {
         ...config.resolve.alias,
-        "@": path.resolve(__dirname, "../"),
+        "@": path.resolve(__dirname, "../src"),
       };
     }
     return config;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 import type { NextConfig } from "next";
+import type { RuleSetRule } from "webpack";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  // SVGR
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule: RuleSetRule) =>
+        rule.test instanceof RegExp && rule.test.test(".svg")
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: {
+          not: [...(fileLoaderRule.resourceQuery?.not || []), /url/],
+        },
+        use: ["@svgr/webpack"],
+      }
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
   reactStrictMode: true,
 };
 
