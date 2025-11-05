faca2b9ee83959b9f9743575241ed4abbd31e962
Chamsol Kim <kcsol1005@gmail.com>
2025-11-05 23:30:00 +0900

    refactor [] Prettier 설정 변경 후 `_app.tsx` import formatting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
+import ReactQueryProvider from "@/providers/ReactQueryProvider";
 import "@/styles/globals.css";
 import "@/styles/tailwind.css";
 import type { AppProps } from "next/app";
-import ReactQueryProvider from "@/providers/ReactQueryProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
