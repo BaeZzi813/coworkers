5a4e242aaf9a44191307a72eb7010e11e0015719
JeongChanMin <setcorin@naver.com>
2025-11-08 18:40:03 +0900

    feat [codeif-fe18-4-3 #12] 인풋 옵션(텍스트, 패스워드, 버튼) 제작

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,17 +1,16 @@
 import { Input as InputComponent } from "@/components/input";
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/nextjs";
 
-const meta: Meta<typeof InputComponent> = {
+const meta = {
   title: "Components/Input",
   component: InputComponent,
   parameters: {
     layout: "centered",
   },
-  tags: ["autodocs"],
   argTypes: {
     size: {
       control: { type: "radio" },
-      options: ["pc", "mobile"],
+      options: ["pc", "mobile", "modal"],
     },
     type: {
       control: { type: "radio" },
@@ -20,26 +19,30 @@ const meta: Meta<typeof InputComponent> = {
     placeholder: {
       control: { type: "text" },
     },
+    variant: {
+      control: { type: "radio" },
+      options: ["default", "password", "passwordChange"],
+    },
   },
   args: {
     placeholder: "이메일을 입력하세요.",
     size: "pc",
     type: "text",
+    variant: "default",
   },
-};
+} satisfies Meta<typeof InputComponent>;
 
 export default meta;
 
-type Story = StoryObj<typeof InputComponent>;
+type Story = StoryObj<typeof meta>;
 
-export const PcInput: Story = {
+export const Input: Story = {
   args: {
     size: "pc",
   },
-};
-
-export const MobileInput: Story = {
-  args: {
-    size: "mobile",
-  },
+  render: (args) => (
+    <div className="flex w-xl flex-col items-center gap-4">
+      <InputComponent {...args} />
+    </div>
+  ),
 };
