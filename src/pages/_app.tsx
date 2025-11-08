ea1e96f513f7dbc27a832dc52a8b4e09a4765731
Chamsol Kim <kcsol1005@gmail.com>
2025-11-08 16:52:03 +0900

    refactor [#7] App root에 modal을 위한 `OverlayProvider` 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,26 @@
 import ReactQueryProvider from "@/providers/ReactQueryProvider";
 import "@/styles/globals.css";
 import "@/styles/tailwind.css";
+import { DehydratedState } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { OverlayProvider } from "overlay-kit";
+import { PropsWithChildren } from "react";
+
+function Providers({
+  reactQueryState,
+  children,
+}: PropsWithChildren<{ reactQueryState: DehydratedState }>) {
+  return (
+    <ReactQueryProvider dehydratedState={reactQueryState}>
+      <OverlayProvider>{children}</OverlayProvider>
+    </ReactQueryProvider>
+  );
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ReactQueryProvider dehydratedState={pageProps.dehydratedState}>
+    <Providers reactQueryState={pageProps.dehydratedState}>
       <Component {...pageProps} />
-    </ReactQueryProvider>
+    </Providers>
   );
 }
