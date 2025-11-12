6cbd28fda61c6596d7cce4bdd8a8608491e30fd1
BaeZzi813 <woehd8252@gmail.com>
2025-11-12 23:42:41 +0900

    Feat [#44] 반응형 css 적용

--- src/features/boards/SearchBar.tsx
+++ src/features/boards/SearchBar.tsx
@@ -8,9 +8,11 @@ interface SearchBarProps {
 export default function SearchBar({ value, onChange }: SearchBarProps) {
   return (
     <>
-      <div className="relative flex items-center justify-between">
-        <div className="text-2xl-b text-text-primary">자유게시판</div>
-        <div className="absolute top-3.5 right-[370px]">
+      <div className="relative mt-[25px] flex h-[92px] w-[343px] flex-col gap-5 tablet:mt-[77px] tablet:h-14 tablet:w-[620px] tablet:flex-row tablet:items-center tablet:justify-between desktop:mt-[87px] desktop:w-[1120px]">
+        <div className="text-xl-b tablet:text-2xl-b text-text-primary">
+          자유게시판
+        </div>
+        <div className="absolute top-13.5 left-3 tablet:top-3.5 tablet:left-[215px] desktop:left-[715px]">
           <Icon name="magnifier" size="large" color="white" />
         </div>
         <input
@@ -18,7 +20,7 @@ export default function SearchBar({ value, onChange }: SearchBarProps) {
           value={value}
           onChange={(e) => onChange(e.target.value)}
           placeholder="검색어를 입력해주세요"
-          className="h-14 w-[420px] rounded-[1000px] border-2 border-brand-primary px-14 focus:outline-none"
+          className="h-12 w-[343px] rounded-[1000px] border-2 border-brand-primary pl-12 focus:outline-none tablet:h-14 tablet:w-[420px]"
         />
       </div>
     </>
