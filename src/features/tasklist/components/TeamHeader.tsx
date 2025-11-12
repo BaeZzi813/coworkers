94b1ef029983aede93271ff433cf64322a8d7513
minyoung1101 <my11612@naver.com>
2025-11-12 20:50:16 +0900

    refactor [#34] 불필요한 Fragment 제거

--- src/features/tasklist/components/TeamHeader.tsx
+++ src/features/tasklist/components/TeamHeader.tsx
@@ -34,37 +34,35 @@ export default function TeamHeader({
   };
 
   return (
-    <>
-      <header className="flex h-7 w-full max-w-[1120px] items-center justify-start rounded-xl bg-none shadow-[0px_15px_50px_-12px_rgba(0,0,0,0.05)] outline-border-primary desktop:h-16 desktop:justify-between desktop:bg-background-primary desktop:px-7 desktop:py-4 desktop:outline">
-        <h1 className="text-lg-b text-text-primary tablet:text-2xl-b">
-          {teamName}
-        </h1>
+    <header className="flex h-7 w-full max-w-[1120px] items-center justify-start rounded-xl bg-none shadow-[0px_15px_50px_-12px_rgba(0,0,0,0.05)] outline-border-primary desktop:h-16 desktop:justify-between desktop:bg-background-primary desktop:px-7 desktop:py-4 desktop:outline">
+      <h1 className="text-lg-b text-text-primary tablet:text-2xl-b">
+        {teamName}
+      </h1>
 
-        <Image
-          src={BgTeamPattern}
-          alt=""
-          aria-hidden
-          width={326}
-          height={102}
-          className="mr-2.5 ml-auto hidden desktop:block"
+      <Image
+        src={BgTeamPattern}
+        alt=""
+        aria-hidden
+        width={326}
+        height={102}
+        className="mr-2.5 ml-auto hidden desktop:block"
+      />
+
+      {isAdmin && (
+        <Dropdown
+          anchor={
+            <button
+              aria-label="팀 설정 열기"
+              className="ml-2 cursor-pointer py-1"
+            >
+              <Icon name="gear" size="large" />
+            </button>
+          }
+          options={options}
+          alignment="left"
+          onSelect={handleSelect}
         />
-
-        {isAdmin && (
-          <Dropdown
-            anchor={
-              <button
-                aria-label="팀 설정 열기"
-                className="ml-2 cursor-pointer py-1"
-              >
-                <Icon name="gear" size="large" />
-              </button>
-            }
-            options={options}
-            alignment="left"
-            onSelect={handleSelect}
-          />
-        )}
-      </header>
-    </>
+      )}
+    </header>
   );
 }
