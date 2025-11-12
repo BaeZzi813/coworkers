f37c5a27d7d350dd4141a2d266bfee7e2867d05b
minyoung1101 <my11612@naver.com>
2025-11-12 13:21:38 +0900

    refactor [#40] DatePickerChevron orientation별 렌더링을 매핑 구조로 개선

--- src/features/date-picker/components/DatePickerChevron.tsx
+++ src/features/date-picker/components/DatePickerChevron.tsx
@@ -18,32 +18,16 @@ export default function DatePickerChevron({
     disabled ? "opacity-30" : "opacity-100",
     className ?? ""
   );
-  switch (orientation) {
-    case "up":
-      return (
-        <button className={clsx(baseStyle, "rotate-180")}>
-          <Icon name="triangleDown" size="large" />
-        </button>
-      );
-    case "down":
-      return (
-        <span className={clsx(baseStyle)}>
-          <Icon name="triangleDown" size="large" />
-        </span>
-      );
-    case "left":
-      return (
-        <span className={clsx(baseStyle, "rotate-90")}>
-          <Icon name="triangleDown" size="large" />
-        </span>
-      );
-    case "right":
-      return (
-        <span className={clsx(baseStyle, "rotate-270")}>
-          <Icon name="triangleDown" size="large" />
-        </span>
-      );
-    default:
-      return <></>;
-  }
+  const rotateStyle = {
+    up: "rotate-180",
+    down: "",
+    left: "rotate-90",
+    right: "rotate-270",
+  };
+
+  return (
+    <span className={clsx(baseStyle, rotateStyle[orientation])}>
+      <Icon name="triangleDown" size="large" />
+    </span>
+  );
 }
