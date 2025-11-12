226c752f82d2ebed9f1daa9f437c2f636ba2fea1
Chamsol Kim <kcsol1005@gmail.com>
2025-11-12 15:13:24 +0900

    fix [#9] Page content가 길어질 때 sidebar를 제외한 main 영역만 scroll 되도록 수정

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -3,9 +3,9 @@ import { PropsWithChildren } from "react";
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <div className="flex">
+    <div className="flex h-screen">
       <Sidebar className="relative z-10" />
-      <main className="grow">{children}</main>
+      <main className="grow overflow-y-scroll">{children}</main>
     </div>
   );
 }
