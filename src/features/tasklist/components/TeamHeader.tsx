9b97931d5a61d4abcf6e20c560980250b8a82e83
minyoung1101 <my11612@naver.com>
2025-11-12 21:12:40 +0900

    refactor [#34] 공통 shadow 유틸리티 클래스(shadow-card) 추가 및 적용

--- src/features/tasklist/components/TeamHeader.tsx
+++ src/features/tasklist/components/TeamHeader.tsx
@@ -34,7 +34,7 @@ export default function TeamHeader({
   };
 
   return (
-    <header className="flex h-7 w-full max-w-[1120px] items-center justify-start rounded-xl bg-none shadow-[0px_15px_50px_-12px_rgba(0,0,0,0.05)] outline-border-primary desktop:h-16 desktop:justify-between desktop:bg-background-primary desktop:px-7 desktop:py-4 desktop:outline">
+    <header className="flex h-7 w-full max-w-[1120px] items-center justify-start rounded-xl bg-none shadow-card outline-border-primary desktop:h-16 desktop:justify-between desktop:bg-background-primary desktop:px-7 desktop:py-4 desktop:outline">
       <h1 className="text-lg-b text-text-primary tablet:text-2xl-b">
         {teamName}
       </h1>
