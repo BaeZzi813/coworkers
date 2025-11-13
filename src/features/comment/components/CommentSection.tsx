6904ec20c1e745884952d0f1f22763cff055811c
minyoung1101 <my11612@naver.com>
2025-11-14 05:07:42 +0900

    feat [#39] CommentSection 컴포넌트 구현

---
+++ src/features/comment/components/CommentSection.tsx
@@ -0,0 +1,37 @@
+import { CommentItemProps } from "@/features/comment/components/CommentItem";
+import CommentList from "@/features/comment/components/CommentList";
+import CommentPost from "@/features/comment/components/CommentPost";
+import clsx from "clsx";
+
+interface CommentSectionProps {
+  comments: CommentItemProps[];
+  onSubmit?: (content: string) => void;
+  onEdit?: (id: number, content: string) => void;
+  onDelete?: (id: number) => void;
+  className?: string;
+  px?: number;
+}
+
+export default function CommentSection({
+  comments,
+  onSubmit,
+  onEdit,
+  onDelete,
+  className,
+  px,
+}: CommentSectionProps) {
+  return (
+    <section className={clsx("flex flex-col gap-4", className)}>
+      <h2 className="text-2xl-b" style={{ paddingLeft: px, paddingRight: px }}>
+        댓글<span className="ml-1 text-brand-primary">{comments.length}</span>
+      </h2>
+      <CommentPost onSubmit={onSubmit} px={px} />
+      <CommentList
+        comments={comments}
+        onEdit={onEdit}
+        onDelete={onDelete}
+        px={px}
+      />
+    </section>
+  );
+}
