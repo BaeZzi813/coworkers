b36bb33de52e325548fee9365b324716dc855512
minyoung1101 <my11612@naver.com>
2025-11-15 07:45:06 +0900

    feat [#35] TaskItem 컴포넌트 구현

---
+++ src/features/tasklist/components/TaskItem.tsx
@@ -0,0 +1,65 @@
+import DoneBadge from "@/components/badge/DoneBadge";
+import Dropdown, { DropdownOption } from "@/components/dropdown";
+import Icon from "@/components/icon";
+import { useResponsive } from "@/hooks/use-responsive";
+import { Task } from "@/types/task";
+import clsx from "clsx";
+
+interface TaskItemProps {
+  title: string;
+  tasks: Task[];
+  onClick: () => void;
+}
+
+export default function TaskItem({ title, tasks, onClick }: TaskItemProps) {
+  const { isDesktop } = useResponsive();
+
+  const totalCount = tasks.length;
+  const doneCount = tasks.filter((task) => task.doneAt).length;
+
+  const options: DropdownOption[] = [
+    { label: "수정하기", value: "edit" },
+    { label: "삭제하기", value: "delete" },
+  ];
+
+  const handleSelect = (option: DropdownOption) => {
+    switch (option.value) {
+      case "edit":
+        break;
+      case "delete":
+        break;
+    }
+  };
+
+  return (
+    <div
+      className={clsx(
+        "flex items-center justify-start",
+        isDesktop &&
+          "h-[54px] cursor-pointer rounded-xl border border-border-primary pr-3 pl-5"
+      )}
+      onClick={onClick}
+    >
+      <span className="text-sm-s desktop:text-md-s">{title}</span>
+      <div className="desktop:ml-auto">
+        <DoneBadge current={doneCount} total={totalCount} size="small" />
+      </div>
+      {isDesktop && (
+        <Dropdown
+          anchor={
+            <button
+              aria-label="댓글 설정 열기"
+              className="cursor-pointer py-1.5"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <Icon name="dots" size="large" />
+            </button>
+          }
+          options={options}
+          alignment="right"
+          onSelect={(option) => handleSelect(option as DropdownOption)}
+        />
+      )}
+    </div>
+  );
+}
