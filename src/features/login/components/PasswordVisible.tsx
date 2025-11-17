eeb8d4eaf09841b1e98b8fdff8bee69c7e1a3c8f
JeongChanMin <setcorin@naver.com>
2025-11-17 20:38:05 +0900

    refactor [#55] 라벨, 패스워드 visible 컴포넌트 작업, login폴더구조 변경, 유효성검사 추가

---
+++ src/features/login/components/PasswordVisible.tsx
@@ -0,0 +1,27 @@
+import InvisibleIcon from "@/assets/icons/ic-invisible.svg";
+import VisibleIcon from "@/assets/icons/ic-visible.svg";
+
+interface PasswordVisibleProps {
+  isVisible: boolean;
+  onToggle: () => void;
+}
+
+export default function PasswordVisible({
+  isVisible,
+  onToggle,
+}: PasswordVisibleProps) {
+  return (
+    <button
+      type="button"
+      onClick={onToggle}
+      className="flex cursor-pointer items-center justify-center"
+      aria-label={isVisible ? "비밀번호 숨기기" : "비밀번호 보기"}
+    >
+      {isVisible ? (
+        <VisibleIcon width={28} height={24} />
+      ) : (
+        <InvisibleIcon width={28} height={24} />
+      )}
+    </button>
+  );
+}
