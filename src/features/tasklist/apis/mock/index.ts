8ece60132f3841e7d57d7f1f5a1b1b238e1fb777
minyoung1101 <my11612@naver.com>
2025-11-18 04:51:38 +0900

    feat [#78] getTaskList에 TaskGroup 변환 및 frequency 타입 매핑 로직 추가

--- src/features/tasklist/apis/mock/index.ts
+++ src/features/tasklist/apis/mock/index.ts
@@ -1,5 +1,12 @@
+import { TaskFrequency, TaskGroup } from "@/types/task";
 import tasksCommentsMock from "./task-list.json";
 
-export async function getTaskList() {
-  return tasksCommentsMock;
+export async function getTaskList(): Promise<TaskGroup[]> {
+  return tasksCommentsMock.map((group) => ({
+    ...group,
+    tasks: group.tasks.map((task) => ({
+      ...task,
+      frequency: task.frequency as TaskFrequency,
+    })),
+  }));
 }
