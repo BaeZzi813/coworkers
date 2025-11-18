962111503ca877f979040d0e9488ce486af8f7c4
minyoung1101 <my11612@naver.com>
2025-11-18 09:43:44 +0900

    feat [#78] TodoList 목데이터 및 getTodoList() 구현

--- src/features/tasklist/apis/mock/index.ts
+++ src/features/tasklist/apis/mock/index.ts
@@ -1,12 +1,20 @@
-import { TaskFrequency, TaskGroup } from "@/types/task";
-import tasksCommentsMock from "./task-list.json";
+import { Task, TaskFrequency, TaskGroup } from "@/types/task";
+import taskListMock from "./task-list.json";
+import todoListMock from "./todo-list.json";
 
 export async function getTaskList(): Promise<TaskGroup[]> {
-  return tasksCommentsMock.map((group) => ({
+  return taskListMock.map((group) => ({
     ...group,
     tasks: group.tasks.map((task) => ({
       ...task,
       frequency: task.frequency as TaskFrequency,
     })),
+  }));
+}
+
+export async function getTodoList(): Promise<Task[]> {
+  return todoListMock.map((task) => ({
+    ...task,
+    frequency: task.frequency as TaskFrequency,
   }));
 }
