95d76219d7beeaacddfaf15516a43b61c9091216
minyoung1101 <my11612@naver.com>
2025-11-19 07:51:40 +0900

    feat [#78] DateNav 스타일 클래스 오타 수정

--- src/features/tasklist/components/DateNav.tsx
+++ src/features/tasklist/components/DateNav.tsx
@@ -18,7 +18,7 @@ export default function DateNav({
 
   return (
     <div className="flex items-center justify-end gap-2">
-      <span className="tabelt:text-lg-m text-sm-m text-text-primary">
+      <span className="text-sm-m text-text-primary tablet:text-lg-m">
         {year}년 {month}월
       </span>
 
