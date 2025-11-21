222ff2bebd1305ee9db0b7da62ad0381836aa8c3
Chamsol Kim <kcsol1005@gmail.com>
2025-11-21 20:25:45 +0900

    feat [#83] Avatar component에 `className`, `style` prop 추가
    외부에서 style을 설정할 수 있는 기능 제공

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -1,12 +1,15 @@
 import PlaceholderLargeImage from "@/assets/images/avatar-placeholder-lg.svg";
 import PlaceholderMediumImage from "@/assets/images/avatar-placeholder-md.svg";
 import PlaceholderSmallImage from "@/assets/images/avatar-placeholder-sm.svg";
+import clsx from "clsx";
 import Image from "next/image";
-import { FC, SVGProps } from "react";
+import { CSSProperties, FC, SVGProps } from "react";
 
 type AvatarSize = "small" | "medium" | "large";
 
 interface Props {
+  className?: string;
+  style?: CSSProperties;
   source?: string;
   size?: AvatarSize;
 }
@@ -29,15 +32,25 @@ const borderRadius: Record<AvatarSize, string> = {
   large: "rounded-[12px]",
 };
 
-export default function Avatar({ source, size = "large" }: Props) {
+export default function Avatar({
+  className,
+  style,
+  source,
+  size = "large",
+}: Props) {
   if (!source) {
     const Placeholder = placeholderImage[size];
-    return <Placeholder width={imageSize[size]} height={imageSize[size]} />;
+    return (
+      <div className={className} style={style}>
+        <Placeholder width={imageSize[size]} height={imageSize[size]} />
+      </div>
+    );
   }
 
   return (
     <Image
-      className={borderRadius[size]}
+      className={clsx(borderRadius[size], className)}
+      style={style}
       src={source}
       alt="User Avatar"
       width={imageSize[size]}
