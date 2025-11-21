a74c7e1b403f9932c3e7cab53dfd0672bc68ab8e
Chamsol Kim <kcsol1005@gmail.com>
2025-11-21 18:26:11 +0900

    feat [#82] Group detail data를 가져오는 API 호출 함수 구현

--- src/features/group/apis/index.ts
+++ src/features/group/apis/index.ts
@@ -1,9 +1,19 @@
 import { apiRequest, type APIRequestOptions } from "@/services/api-request";
-import { UserGroup } from "@/types/group";
+import { Group, UserGroup } from "@/types/group";
 
 export async function getUserGroups(options?: APIRequestOptions) {
   return apiRequest<UserGroup[]>(async (instance) => {
     const response = await instance.get<UserGroup[]>("/user/groups");
+    return response.data;
+  }, options);
+}
+
+export async function getGroup(
+  params: { groupId: number },
+  options?: APIRequestOptions
+) {
+  return apiRequest<Group>(async (instance) => {
+    const response = await instance.get(`/groups/${params.groupId}`);
     return response.data;
   }, options);
 }
