a48ecdc8f28335194135290fcf81add9263a91f1
Chamsol Kim <kcsol1005@gmail.com>
2025-11-21 18:34:48 +0900

    feat [#82] Team page에 SSR 적용

--- src/pages/[teamId]/index.tsx
+++ src/pages/[teamId]/index.tsx
@@ -1,3 +1,57 @@
-export default function TeamPage() {
-  return <h1>Team Page</h1>;
+import { getGroup } from "@/features/group/apis";
+import {
+  GSSP_NOT_FOUND_RETURN,
+  gsspPropsWithTokenReturn,
+} from "@/libs/ssr/gssp-return";
+import {
+  gsspWithAuth,
+  serverSideComponentWithAuth,
+} from "@/libs/ssr/with-auth";
+import { Member } from "@/types/member";
+import { Task, TaskList } from "@/types/task";
+
+interface PageProps {
+  members: Member[];
+  taskLists: TaskList[];
+  tasks: Task[];
 }
+
+export const getServerSideProps = gsspWithAuth(async (context, accessToken) => {
+  const params = context.params;
+  const teamId = Number(params?.teamId);
+  if (isNaN(teamId)) {
+    return GSSP_NOT_FOUND_RETURN;
+  }
+
+  const group = await getGroup({ groupId: teamId }, { accessToken });
+
+  return gsspPropsWithTokenReturn(
+    {
+      members: group.members,
+      taskLists: group.taskLists,
+      tasks: group.taskLists.flatMap((taskList) => taskList.tasks),
+    },
+    accessToken
+  );
+});
+
+export default serverSideComponentWithAuth<PageProps>(
+  ({ members, taskLists, tasks }) => {
+    return (
+      <div>
+        <div>
+          <div>Members</div>
+          {members.map((member) => member.userName)}
+        </div>
+        <div>
+          <div>Task Lists</div>
+          {taskLists.map((taskList) => taskList.name)}
+        </div>
+        <div>
+          <div>Tasks</div>
+          {tasks.map((task) => task.name)}
+        </div>
+      </div>
+    );
+  }
+);
