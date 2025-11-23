a98d47eba6a71ee00e8e99bce3d5b3aa3a09e240
Chamsol Kim <kcsol1005@gmail.com>
2025-11-23 21:52:25 +0900

    feat [#84] Group 추가, 수정, 삭제 API 호출 함수 구현

--- src/features/group/apis/index.ts
+++ src/features/group/apis/index.ts
@@ -1,4 +1,6 @@
+import { uploadImage } from "@/features/image/apis";
 import { apiRequest, type APIRequestOptions } from "@/services/api-request";
+import { clientApiInstance } from "@/services/instance/client";
 import { Group, UserGroup } from "@/types/group";
 
 export async function getUserGroups(options?: APIRequestOptions) {
@@ -8,12 +10,72 @@ export async function getUserGroups(options?: APIRequestOptions) {
   }, options);
 }
 
+interface GetGroupParams {
+  groupId: number;
+}
+
 export async function getGroup(
-  params: { groupId: number },
+  params: GetGroupParams,
   options?: APIRequestOptions
 ) {
   return apiRequest<Group>(async (instance) => {
     const response = await instance.get(`/groups/${params.groupId}`);
     return response.data;
   }, options);
+}
+
+export interface PostGroupParams {
+  imageFile?: File;
+  name: string;
+}
+
+export async function postGroup({ imageFile, name }: PostGroupParams) {
+  const params: {
+    image?: string;
+    name: string;
+  } = { name };
+
+  if (imageFile) {
+    const { url } = await uploadImage({ imageFile });
+    params.image = url;
+  }
+
+  const response = await clientApiInstance.post<Group>("/groups", params);
+  return response.data;
+}
+
+export interface PatchGroupsParams {
+  groupId: number;
+  imageFile?: File;
+  name?: string;
+}
+
+export async function patchGroup({
+  groupId,
+  imageFile,
+  name,
+}: PatchGroupsParams) {
+  const params: {
+    image?: string;
+    name?: string;
+  } = { name };
+
+  if (imageFile) {
+    const { url } = await uploadImage({ imageFile });
+    params.image = url;
+  }
+
+  const response = await clientApiInstance.patch<Group>(
+    `/groups/${groupId}`,
+    params
+  );
+  return response.data;
+}
+
+export interface DeleteGroupParams {
+  groupId: number;
+}
+
+export async function deleteGroup({ groupId }: DeleteGroupParams) {
+  await clientApiInstance.delete(`/groups/${groupId}`);
 }
