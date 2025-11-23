5e057e0c070ea8e1d735145744196202e85b510d
Chamsol Kim <kcsol1005@gmail.com>
2025-11-23 20:47:40 +0900

    feat [#82] 팀 페이지에서 group 및 user query에 대해 query prefetch 및 dehydrate 적용

--- src/pages/[teamId]/index.tsx
+++ src/pages/[teamId]/index.tsx
@@ -1,5 +1,5 @@
-import { getGroup } from "@/features/group/apis";
-import { getUser } from "@/features/user/apis/get-user";
+import { prefetchGroup, useGroup } from "@/features/group/query";
+import { prefetchUser, useUser } from "@/features/user/query";
 import {
   GSSP_NOT_FOUND_RETURN,
   gsspPropsWithTokenReturn,
@@ -8,17 +8,7 @@ import {
   gsspWithAuth,
   serverSideComponentWithAuth,
 } from "@/libs/ssr/with-auth";
-import { Group } from "@/types/group";
-import { Member } from "@/types/member";
-import { Task, TaskList } from "@/types/task";
-
-interface PageProps {
-  isAdmin: boolean;
-  group: Group;
-  members: Member[];
-  taskLists: TaskList[];
-  tasks: Task[];
-}
+import { dehydrate, QueryClient } from "@tanstack/react-query";
 
 export const getServerSideProps = gsspWithAuth(async (context, accessToken) => {
   const params = context.params;
@@ -27,42 +17,48 @@ export const getServerSideProps = gsspWithAuth(async (context, accessToken) => {
     return GSSP_NOT_FOUND_RETURN;
   }
 
-  const group = await getGroup({ groupId: teamId }, { accessToken });
-  const currentUser = await getUser({ accessToken });
-  const adminMember = group.members.find((member) => member.role === "ADMIN");
-  const isAdmin = adminMember?.userId === currentUser.id;
-
-  return gsspPropsWithTokenReturn(
-    {
-      isAdmin,
-      group,
-      members: group.members,
-      taskLists: group.taskLists,
-      tasks: group.taskLists.flatMap((taskList) => taskList.tasks),
-    },
-    accessToken
-  );
+  const queryClient = new QueryClient();
+  await prefetchGroup(queryClient, { groupId: teamId, accessToken });
+  await prefetchUser(queryClient, { accessToken });
+  return gsspPropsWithTokenReturn({
+    props: { groupId: teamId },
+    dehydratedState: dehydrate(queryClient),
+    accessToken,
+  });
 });
 
-export default serverSideComponentWithAuth<PageProps>(
-  ({ isAdmin, group, members, taskLists, tasks }) => {
-    return (
-      <div>
-        <div>{isAdmin ? "You are an admin" : "You are not an admin"}</div>
-        <div>Group Name : {group.name}</div>
-        <div>
-          <div>Members</div>
-          {members.map((member) => member.userName)}
-        </div>
-        <div>
-          <div>Task Lists</div>
-          {taskLists.map((taskList) => taskList.name)}
-        </div>
-        <div>
-          <div>Tasks</div>
-          {tasks.map((task) => task.name)}
-        </div>
-      </div>
-    );
+interface PageProps {
+  groupId: number;
+}
+
+export default serverSideComponentWithAuth<PageProps>(({ groupId }) => {
+  const { group } = useGroup({ groupId });
+  const { user } = useUser();
+  const adminMember = group?.members.find((member) => member.role === "ADMIN");
+
+  if (!group || !user || !adminMember) {
+    return <div>Loading</div>;
   }
-);
+
+  const isAdmin = adminMember.userId === user.id;
+  const tasks = group.taskLists.flatMap((taskList) => taskList.tasks);
+
+  return (
+    <div>
+      <div>{isAdmin ? "You are an admin" : "You are not an admin"}</div>
+      <div>Group Name : {group.name}</div>
+      <div>
+        <div>Members</div>
+        {group.members.map((member) => member.userName)}
+      </div>
+      <div>
+        <div>Task Lists</div>
+        {group.taskLists.map((taskList) => taskList.name)}
+      </div>
+      <div>
+        <div>Tasks</div>
+        {tasks.map((task) => task.name)}
+      </div>
+    </div>
+  );
+});
