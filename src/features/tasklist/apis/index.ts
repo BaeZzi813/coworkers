1d8a11633ae7975bd679697907cfc46d7e42d7aa
Chamsol Kim <kcsol1005@gmail.com>
2025-11-25 13:17:53 +0900

    feat [#86] TaskList 수정 API 호출 및 mutation 구현

--- src/features/tasklist/apis/index.ts
+++ src/features/tasklist/apis/index.ts
@@ -13,6 +13,24 @@ export async function postTaskList({ groupId, name }: PostTaskListParams) {
   return response.data;
 }
 
+interface PatchTaskListParams {
+  groupId: number;
+  taskListId: number;
+  name: string;
+}
+
+export async function patchTaskList({
+  groupId,
+  taskListId,
+  name,
+}: PatchTaskListParams) {
+  const response = await clientApiInstance.patch(
+    `/groups/${groupId}/task-lists/${taskListId}`,
+    { name }
+  );
+  return response.data;
+}
+
 export interface DeleteTaskListParams {
   groupId: number;
   taskListId: number;
--- src/features/tasklist/query/use-task-list-mutation.tsx
+++ src/features/tasklist/query/use-task-list-mutation.tsx
@@ -1,14 +1,18 @@
 import { useMutation } from "@tanstack/react-query";
-import { deleteTaskList, postTaskList } from "../apis";
+import { deleteTaskList, patchTaskList, postTaskList } from "../apis";
 
 export function useTaskListMutation() {
   const postMutation = useMutation({
     mutationFn: postTaskList,
   });
 
+  const patchMutation = useMutation({
+    mutationFn: patchTaskList,
+  });
+
   const deleteMutation = useMutation({
     mutationFn: deleteTaskList,
   });
 
-  return { postMutation, deleteMutation };
+  return { postMutation, patchMutation, deleteMutation };
 }
