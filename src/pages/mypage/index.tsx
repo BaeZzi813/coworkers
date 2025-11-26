ccab93fae375c1e3e79bb2c943c9dadebbf34e36
JeongChanMin <setcorin@naver.com>
2025-11-26 16:06:07 +0900

    refactor [#93] pr피드백 반영, 비밀번호 변경, 회원탈퇴 모달 ui구현

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,22 +1,114 @@
-import SecessionIcon from "@/assets/icons/ic-secession.svg";
 import { Button } from "@/components/button";
+import Icon from "@/components/icon";
+import { Alert } from "@/components/modal";
 import InputLabel from "@/features/login/components/InputLabel";
 import TeamEditImageInput from "@/features/team/components/TeamEditImageInput";
+import { overlay } from "overlay-kit";
 import { useState } from "react";
 
 export default function MyPage() {
   const [name, setName] = useState("찬민테스트");
   const [email] = useState("chanmin@text.com");
-  const [password, setPassword] = useState("테스트 비밀번호");
+
+  const handleChangePasswordClick = () => {
+    overlay.open(
+      ({ isOpen, close, unmount }) => (
+        <Alert
+          isOpen={isOpen}
+          onClose={close}
+          onExit={unmount}
+          title="비밀번호 변경하기"
+          content={
+            <div className="flex flex-col gap-10">
+              <div className="h-16">
+                <InputLabel
+                  label="새 비밀번호"
+                  id="newPassword"
+                  type="password"
+                  placeholder="새 비밀번호를 입력해주세요."
+                  size="large"
+                />
+              </div>
+
+              <div className="h-20">
+                <InputLabel
+                  label="새 비밀번호 확인"
+                  id="confirmNewPassword"
+                  type="password"
+                  placeholder="다시 한 번 입력해주세요."
+                  size="large"
+                />
+              </div>
+            </div>
+          }
+          actions={[
+            <Button
+              key="close"
+              title="닫기"
+              variant="outlinedPrimary"
+              size="large"
+              isFullWidth={true}
+              onClick={close}
+            />,
+            <Button
+              key="change"
+              title="변경하기"
+              variant="primary"
+              size="large"
+              isFullWidth={true}
+              onClick={close}
+            />,
+          ]}
+        />
+      ),
+      { overlayId: "change-password-alert" }
+    );
+  };
+
+  const handleMembershipWithdrawalClick = () => {
+    overlay.open(
+      ({ isOpen, close, unmount }) => (
+        <Alert
+          isOpen={isOpen}
+          onClose={close}
+          onExit={unmount}
+          header={
+            <div className="flex h-6 w-6 items-center justify-center rounded-full bg-status-danger">
+              <Icon name="alert" size="large" color="white" />
+            </div>
+          }
+          title="회원 탈퇴를 진행하시겠어요?"
+          message="그룹장으로 있는 그룹은 자동으로 삭제되고, 모든 그룹에서 나가집니다."
+          actions={[
+            <Button
+              key="close"
+              title="닫기"
+              variant="outlinedPrimary"
+              size="large"
+              isFullWidth={true}
+              onClick={close}
+            />,
+            <Button
+              key="secession"
+              title="회원 탈퇴"
+              variant="danger"
+              size="large"
+              isFullWidth={true}
+              onClick={close}
+            />,
+          ]}
+        />
+      ),
+      { overlayId: "secession-alert" }
+    );
+  };
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-background-secondary">
-      <div className="relative h-187 w-235 rounded-2xl border border-border-primary bg-background-primary">
-        <div className="absolute top-16 left-14 h-6 w-27">
-          <h1 className="text-2xl-b text-text-primary">계정 설정</h1>
-        </div>
+    <div className="flex h-screen items-center justify-center bg-background-secondary">
+      <div className="flex h-195 w-230 flex-col rounded-2xl border border-border-primary bg-background-primary px-18 pt-16 pb-16">
+        <h1 className="mb-10 text-2xl-b text-text-primary">계정 설정</h1>
 
-        <div className="absolute top-32 left-1/2 -translate-x-1/2">
+        <div className="mb-9 flex justify-center">
           <TeamEditImageInput
             onChange={(file) => {
               console.log(file);
@@ -24,59 +116,53 @@ export default function MyPage() {
           />
         </div>
 
-        <div className="absolute top-72 left-18 h-71 w-198">
-          <div className="flex flex-col gap-6">
-            <div className="h-20">
-              <InputLabel
-                label="이름"
-                id="name"
-                type="text"
-                placeholder="이름을 입력해주세요."
-                size="large"
-                value={name}
-                onChange={(event) => setName(event.target.value)}
-              />
-            </div>
+        <div className="flex flex-col gap-6">
+          <InputLabel
+            label="이름"
+            id="name"
+            type="text"
+            placeholder="이름을 입력해주세요."
+            size="large"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
 
-            <div className="h-20">
-              <InputLabel
-                label="이메일"
-                id="email"
-                type="email"
-                placeholder="이메일을 입력해주세요."
-                size="large"
-                value={email}
-                disabled
-              />
-            </div>
+          <InputLabel
+            label="이메일"
+            id="email"
+            type="email"
+            placeholder="이메일을 입력해주세요."
+            size="large"
+            value={email}
+            disabled
+          />
 
-            <div className="h-20">
-              <InputLabel
-                label="비밀번호"
-                id="password"
-                type="password"
-                placeholder="비밀번호를 입력해주세요."
-                size="large"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-                trailing={
-                  <Button
-                    title="변경하기"
-                    variant="primary"
-                    size="small"
-                    isFullWidth={false}
-                    onClick={() => console.log("비밀번호 변경")}
-                  />
-                }
+          <InputLabel
+            label="비밀번호"
+            id="password"
+            type="password"
+            size="large"
+            value="********"
+            disabled
+            trailing={
+              <Button
+                title="변경하기"
+                variant="primary"
+                size="small"
+                isFullWidth={false}
+                onClick={handleChangePasswordClick}
               />
-            </div>
+            }
+          />
 
-            <div className="mt-1 flex items-center gap-2">
-              <button className="flex h-6 w-32 cursor-pointer items-center text-left text-lg-m text-status-danger hover:text-lg-s hover:underline">
-                <SecessionIcon width={24} height={24} />
-                회원 탈퇴하기
-              </button>
-            </div>
+          <div className="mt-1 flex items-center gap-2">
+            <button
+              className="flex h-6 w-32 cursor-pointer items-center text-left text-lg-m text-status-danger hover:text-lg-s hover:underline"
+              onClick={handleMembershipWithdrawalClick}
+            >
+              <Icon name="secession" size="large" />
+              회원 탈퇴하기
+            </button>
           </div>
         </div>
       </div>
