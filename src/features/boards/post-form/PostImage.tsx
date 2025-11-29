95775989813c78eba288b55e76b221358d895cfc
BaeZzi813 <woehd8252@gmail.com>
2025-11-29 19:08:40 +0900

    Styles [#47] 게시글 작성/수정 이미지Input 시안1 ui 적용

--- src/features/boards/post-form/PostImage.tsx
+++ src/features/boards/post-form/PostImage.tsx
@@ -15,14 +15,34 @@ export default function PostImage({ preview, onChange, onDelete }: Props) {
   return (
     <div className="flex flex-col gap-2 tablet:gap-3">
       <span className="text-md-b tablet:text-lg-b">이미지</span>
-      <div className="flex gap-3">
-        <label className="flex h-20 w-20 cursor-pointer flex-col items-center justify-center gap-1 rounded-xl border border-border-primary hover:bg-slate-100 tablet:h-[120px] tablet:w-[120px]">
+      {preview ? (
+        <div className="relative h-40 w-40 tablet:h-60 tablet:w-60">
+          <Image
+            src={preview}
+            alt="게시글 이미지"
+            fill
+            className="object-covers inset-0 rounded-xl"
+          />
+          <div className="absolute inset-0 rounded-xl bg-black opacity-40" />
+          <button
+            onClick={onDelete}
+            className="absolute top-1/2 left-1/2 flex h-10 w-10 -translate-x-1/2 -translate-y-1/2 cursor-pointer items-center justify-center"
+          >
+            <Icon
+              name="xmark"
+              size={isMobile ? "medium" : "large"}
+              color="white"
+            />
+          </button>
+        </div>
+      ) : (
+        <label className="flex h-40 w-40 cursor-pointer flex-col items-center justify-center gap-1 rounded-xl border border-border-primary hover:bg-slate-100 tablet:h-60 tablet:w-60">
           <Icon
             name="plus"
             color="#9ca3af"
             size={isMobile ? "medium" : "large"}
           />
-          <span className="text-xs-m text-text-default tablet:text-md-m">
+          <span className="text-md-r text-text-default tablet:text-lg-r">
             이미지 등록
           </span>
           <input
@@ -32,23 +52,7 @@ export default function PostImage({ preview, onChange, onDelete }: Props) {
             onChange={onChange}
           />
         </label>
-        {preview && (
-          <div className="relative h-20 w-20 tablet:h-[120px] tablet:w-[120px]">
-            <Image
-              src={preview}
-              alt="게시글 이미지"
-              fill
-              className="rounded-xl object-cover"
-            />
-            <button
-              onClick={onDelete}
-              className="absolute -top-1.5 -right-1.5 flex h-5 w-5 cursor-pointer items-center justify-center rounded-full border border-border-secondary bg-background-primary tablet:h-6 tablet:w-6"
-            >
-              <Icon name="xmark" size={isMobile ? "small" : "medium"} />
-            </button>
-          </div>
-        )}
-      </div>
+      )}
     </div>
   );
 }
